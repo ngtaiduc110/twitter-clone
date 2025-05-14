0626ca2c8cd9f79675c33c1c962e1e6c8c94d684
ngtaiduc110 <ndangkhoi560@gmail.com>
2025-05-14 22:12:40 +0700

    update client and router pages

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -4,14 +4,16 @@ import jwt from "jsonwebtoken"
 export const protectRoute = async (req, res, next) => {
     try {
         const token = req.cookies.jwt;
+
         if (!token) {
             return res.status(401).json({ error: "Unauthorized: No Token Provided" });
         }
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
         if (!decoded) {
             return res.status(401).json({ error: "Unauthorized: Invalid Token" });
         }
-        const user = await User.findById(decoded.userId).select("-password");
+        const user = await User.findById(decoded.id).select("-password");
         if (!user) {
             return res.status(401).json({ error: "Unauthorized: User Not Found" });
         }
